b5f1db9bbb8793d18cbcb484e662d128ba624b30
kcelia <kherfallah_celia@hotmail.fr>
2019-01-18 18:47:49 +0100

    File for drawing dendrogram

---
+++ tools_dendrogram.py
@@ -0,0 +1,65 @@
+#inspired from : https://joernhees.de/blog/2015/08/26/scipy-hierarchical-clustering-and-dendrogram-tutorial/
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from scipy.spatial.distance import pdist
+from scipy.cluster.hierarchy import cophenet
+from scipy.cluster.hierarchy import fcluster
+from scipy.cluster.hierarchy import inconsistent
+from scipy.cluster.hierarchy import dendrogram, linkage
+
+
+def fancy_dendrogram(*args, **kwargs):
+    max_d = kwargs.pop('max_d', None)
+    if max_d and 'color_threshold' not in kwargs:
+        kwargs['color_threshold'] = max_d
+    annotate_above = kwargs.pop('annotate_above', 0)
+
+    ddata = dendrogram(*args, **kwargs)
+
+    if not kwargs.get('no_plot', False):
+        plt.title('Hierarchical Clustering Dendrogram (truncated)')
+        plt.xlabel('sample index or (cluster size)')
+        plt.ylabel('distance')
+        for i, d, c in zip(ddata['icoord'], ddata['dcoord'], ddata['color_list']):
+            x = 0.5 * sum(i[1:3])
+            y = d[1]
+            if y > annotate_above:
+                plt.plot(x, y, 'o', c=c)
+                plt.annotate("%.3g" % y, (x, y), xytext=(0, -5),
+                             textcoords='offset points',
+                             va='top', ha='center')
+        if max_d:
+            plt.axhline(y=max_d, c='k')
+    return ddata
+
+
+
+def estimate_nb_cluster(Z):
+    last = Z[-10:, 2]
+    last_rev = last[::-1]
+    idxs = np.arange(1, len(last) + 1)
+    plt.plot(idxs, last_rev, label='ELBOW')
+
+    acceleration = np.diff(last, 2)  # 2nd derivative of the distances
+    acceleration_rev = acceleration[::-1]
+    plt.plot(idxs[:-2] + 1, acceleration_rev, label='Acceleration of ELBOW')
+    plt.title("nb clusters estimation whith ELBOW")
+    plt.legend()
+    plt.show()
+    k = acceleration_rev.argmax() + 2  # if idx 0 is the max of this we want 2 clusters
+    return k
+
+
+def display_dendro(Z):
+    plt.figure(figsize=(10, 10))
+    plt.title('Hierarchical Clustering Dendrogram')
+    plt.xlabel('sample index')
+    plt.ylabel('distance')
+    dendrogram(
+        Z,
+        leaf_rotation=90.,  # rotates the x axis labels
+        leaf_font_size=8.,  # font size for the x axis labels
+    )
+    plt.show()
